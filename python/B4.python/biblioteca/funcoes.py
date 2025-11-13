c36a594f1a9681d68dde34f59c92807be02686ea
Matheus Henrique <informaticamatheushenrique@gmail.com>
2025-11-13 17:52:49 -0300

    oi

--- python/B4.python/biblioteca/funcoes.py
+++ python/B4.python/biblioteca/funcoes.py
@@ -1,25 +1,29 @@
 
 """
-def letra_maiusculas(palavra):
-
 def letra_minuscula():
 
 def conta_letras():
 
 def maiusculas_1():
 
-
+banana
 """
 
+def letra_maiusculas(palavra):
 
+    maiusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-X = "plava"
-y = ""
+    minusculas = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-m = []
-P = []
+    for letra in palavra:
 
+        for mi in minusculas:
+            for ma in maiusculas:
+                if letra in minusculas[mi]:
+                    letra = maiusculas [ma]
 
-for letra in X:
-    y = (letra)
-    print(y)
+    print(letra)
+
+palavra = input("digite uma palavra: ")
+
+letra_maiusculas(palavra)
