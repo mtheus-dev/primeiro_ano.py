8b981a29ea69334a8f9f162da8873d23071fea7e
Matheus Henrique <informaticamatheushenrique@gmail.com>
2025-11-16 23:52:31 -0400

    oi

--- python/B4.python/biblioteca/funcoes.py
+++ python/B4.python/biblioteca/funcoes.py
@@ -1,14 +1,3 @@
-
-"""
-def letra_minuscula():
-
-def conta_letras():
-
-def maiusculas_1():
-
-banana
-"""
-
 def letra_maiusculas(palavra):
 
     maiusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -19,7 +8,7 @@ def letra_maiusculas(palavra):
 
     for letra in palavra:
         trocou = False
-        for i in range (len(minusculas)):#posição da minha letra
+        for i in range (len(minusculas)):
             
             if letra == minusculas[i]:
 
@@ -27,9 +16,66 @@ def letra_maiusculas(palavra):
                 trocou = True
                 break
 
-            if not trocou:
-                resultado += letra
+        if not trocou:
+            resultado += letra
                 
     print(resultado)
 
-letra_maiusculas("palavra")
+def letra_minuscula(palavra):
+
+    maiusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+    minusculas = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+    resultado = ""
+
+    for letra in palavra:
+        trocou = False
+        for i in range (len(maiusculas)):#posição da minha letra
+            
+            if letra == maiusculas[i]:
+
+                resultado += minusculas[i]
+                trocou = True
+                break
+
+        if not trocou:
+            resultado += letra
+
+    print(resultado)
+
+def conta_letras(palavra):
+
+    contador = 0
+
+    for i in palavra:
+        
+        contador += 1
+
+    print(f"Na palavra {palavra} tem {contador} letras")
+
+
+def primeira_letra_m(palavra):
+
+    maiusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+    minusculas = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+    resultado = ""
+
+    for letra in palavra:
+        trocou = False
+        for i in range (len(minusculas)):
+            
+            if letra == minusculas[i]:
+
+                resultado += (maiusculas[i])
+                trocou = True
+                break
+
+        if not trocou:
+            resultado += letra
+                
+    print(resultado)
+
+primeira_letra_m("banana")
